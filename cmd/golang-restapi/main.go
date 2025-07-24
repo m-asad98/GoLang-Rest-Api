22d6d5082acc5d427246061765d2f63c5f795cef
m-asad98 <muhammad.asad6996@gmail.com>
2025-07-24 23:32:29 +0500

    Graceful shutdown added

--- cmd/golang-restapi/main.go
+++ cmd/golang-restapi/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/m-asad98/golang-restapi/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to REST API")
+
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Golang REST API"))
+	})
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server started ", slog.String("address", cfg.Addr))
+	//fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Failed to shut down server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully")
+
 }
