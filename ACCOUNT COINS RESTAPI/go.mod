a9f5ef0bb21036474517b186f9a41400adcd9569
m-asad98 <muhammad.asad6996@gmail.com>
2025-08-25 01:08:44 +0500

    Added Database function

--- ACCOUNT COINS RESTAPI/go.mod
+++ ACCOUNT COINS RESTAPI/go.mod
@@ -2,4 +2,8 @@ module github.com/m-asad98/GO-API-AccountsCoins
 
 go 1.24.5
 
-require github.com/go-chi/chi/v5 v5.2.2 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- ACCOUNT COINS RESTAPI/internal/middleware/authorization.go
+++ ACCOUNT COINS RESTAPI/internal/middleware/authorization.go
@@ -1,1 +1,44 @@
 package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/m-asad98/GO-API-AccountsCoins/api"
+	"github.com/m-asad98/GO-API-AccountsCoins/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
+
+var UnAuthorizedError = errors.New("Invalid username or token")
+
+func Authorization(next http.Handler) http.Handler {
+	return http.handlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+
+		if username == "" || token == "" {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+
+		var database *tools.DatabaseInterface
+		database, err = tools.NewDatabase() 
+		if err != nil {
+			api.InternalErrorHandler(w)
+			return
+		}
+
+		var loginDetails *tools.loginDetails
+		loginDetails = (*database).GetUserLoginDetails(username)
+
+		if (loginDetails == nil || (token != (*loginDetails).Authtoken)) {
+			log.Error(UnAuthorizedError)
+		)
+		}
+
+	}
+
+}
